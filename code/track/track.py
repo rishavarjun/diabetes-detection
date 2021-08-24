2d6898dc065a7f15bb4476e9e61900c6cb696daa
pradeepgaur <pradeepcitm@gmail.com>
2021-08-24 23:23:09 +0530

    BUG FIX: trying to display accuracy

--- code/track/track.py
+++ code/track/track.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
     
     #print(f"::set-output name=run_metrics_markdown::{run_metrics_markdown}")
 
-    print(f"::set-output name=pradeep_fresh_data_accuracy::23")
+    #print(f"::set-output name=pradeep_fresh_data_accuracy::23")
     #run.log(f"::set-output name=pradeep_fresh_data_accuracy::{accuracy}")
     
     #print(f"::set-output name=pradeep_fresh_data_accuracy::{accuracy}")
