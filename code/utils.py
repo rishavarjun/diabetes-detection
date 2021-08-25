aafd1230113158bc9349093b6202254017ce8c3c
pradeepgaur <pradeepcitm@gmail.com>
2021-08-25 22:49:50 +0530

    BUG FIX: trying to display accuracy

--- code/utils.py
+++ code/utils.py
@@ -3,7 +3,7 @@ args = sys.argv
 
 arr = args[1].split(':')
 input_string = ':'.join(arr[1:])
-chars_to_remove = "* '"
+chars_to_remove = "* '\""
 
 for character in chars_to_remove:
     input_string = input_string.replace(character, "")
@@ -14,8 +14,8 @@ arr_keyval = cleaned_string.split(",")
 dictionary = {}
 for keyval in arr_keyval:
     temp_Arr = keyval.split(":")
-    #dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
-    dictionary[temp_Arr[0]] = temp_Arr[1]
+    dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
+    #dictionary[temp_Arr[0]] = temp_Arr[1]
 
-#print(dictionary[args[2]])
-print(dictionary)
+print(dictionary[args[2]])
+#print(dictionary)
