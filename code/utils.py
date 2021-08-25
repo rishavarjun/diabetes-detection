2c6b3a01c97606c854f5fc06cd8923c97f7f0d43
pradeepgaur <pradeepcitm@gmail.com>
2021-08-26 00:25:18 +0530

    BUG FIX: trying to display accuracy

--- code/utils.py
+++ code/utils.py
@@ -1,10 +1,11 @@
 import sys
+import re
 
 args = sys.argv
 
 
-print("Args =",args)
-print(args)
+#print("Args =",args)
+#print(args)
 
 arr = args[1].split(':')
 input_string = ':'.join(arr[1:])
@@ -15,16 +16,19 @@ for character in chars_to_remove:
 
 cleaned_string = input_string
 
-print("cleaned string =",cleaned_string)
+#print("cleaned string =",cleaned_string)
 arr_keyval = cleaned_string.split(",")
 
 dictionary = {}
 for keyval in arr_keyval:
     temp_Arr = keyval.split(":")
-    dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
+    result = re.sub('/[^0-9.]/g', '', temp_Arr[1])
+    dictionary[temp_Arr[0]] = float(result) if result.isnumeric else result
     #dictionary[temp_Arr[0]] = temp_Arr[1]
 
-print("dictionary =",dictionary)
+#print("dictionary =",dictionary)
 
-print("Output =", dictionary[args[2]])
-#print(dictionary)
+#print("Output =", dictionary[args[2]])
+print(dictionary)
+
+#print(dictionary[args[2]])
