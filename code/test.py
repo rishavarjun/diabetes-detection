f6e83224229bab8dd1e1d2007eae2b6422c5f7a6
pradeepgaur <pradeepcitm@gmail.com>
2021-08-26 01:30:55 +0530

    BUG FIX: trying to display accuracy

--- code/test.py
+++ code/test.py
@@ -1,20 +1,22 @@
-import sys
 import re
 
-args = sys.argv
+str = "***'diabetes-detection-monitor-via-aml_1629920362_04ed9ee5': ***'testing_value': 15, 'pradeep_fresh_data_accuracy': 0.456***"
 
-str = args[1]
+#result = re.sub('/[^0-9.]/g', '', str)
 
 list_of_char = ["*", " ", "'"]
 pattern = '[' + ''.join(list_of_char) + ']'
 # Remove characters matched by pattern
 result = re.sub(pattern, '', str)
+
 #--
+
 arr = result.split(':')
 input_string = ':'.join(arr[1:])
 
 cleaned_string = input_string
 
+#print("cleaned string =",cleaned_string)
 arr_keyval = cleaned_string.split(",")
 
 dictionary = {}
@@ -28,4 +30,4 @@ for keyval in arr_keyval:
 
     dictionary[temp_Arr[0]] = result
     
-print(dictionary[args[2]])
+print(dictionary["pradeep_fresh_data_accuracy"])
