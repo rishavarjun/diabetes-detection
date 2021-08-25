1c882fc6494e8cf4de02f9f5a82964f373c4a6d1
pradeepgaur <pradeepcitm@gmail.com>
2021-08-25 19:24:12 +0530

    BUG FIX: trying to display accuracy

--- code/utils.py
+++ code/utils.py
@@ -14,6 +14,8 @@ arr_keyval = cleaned_string.split(",")
 dictionary = {}
 for keyval in arr_keyval:
     temp_Arr = keyval.split(":")
-    dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
+    #dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
+    dictionary[temp_Arr[0]] = temp_Arr[1]
 
-print(dictionary[args[2]])
+#print(dictionary[args[2]])
+print(dictionary)
