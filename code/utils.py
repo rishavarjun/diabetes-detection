1c337166832cf1cc3b50dfe9aa701b7895f36168
pradeepgaur <pradeepcitm@gmail.com>
2021-08-25 19:12:08 +0530

    BUG FIX: trying to display accuracy

--- code/utils.py
+++ code/utils.py
@@ -1,9 +1,5 @@
-#python code/utils.py 
-
 import sys
 args = sys.argv
-
-#args = ["aasa","***diabetes-detection-monitor-via-aml_1629891337_5975a671: ***testing_value: 15, pradeep_fresh_data_accuracy: 0.456***", "pradeep_fresh_data_accuracy"]
 
 arr = args[1].split(':')
 input_string = ':'.join(arr[1:])
@@ -21,4 +17,3 @@ for keyval in arr_keyval:
     dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
 
 print(dictionary[args[2]])
-
