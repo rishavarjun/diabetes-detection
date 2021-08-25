10224a7e3e81b0414812bc0622cdc37c6ad0490e
pradeepgaur <pradeepcitm@gmail.com>
2021-08-25 18:02:52 +0530

    BUG FIX: trying to display accuracy

---
+++ code/utils.py
@@ -0,0 +1,23 @@
+#python code/utils.py 
+
+import sys
+args = sys.argv
+
+#args = ["aasa","***diabetes-detection-monitor-via-aml_1629891337_5975a671: ***testing_value: 15, pradeep_fresh_data_accuracy: 0.456***", "pradeep_fresh_data_accuracy"]
+
+arr = args[1].split(':')
+input_string = ':'.join(arr[1:])
+chars_to_remove = "* "
+
+for character in chars_to_remove:
+  input_string = input_string.replace(character, "")
+
+cleaned_string = input_string
+arr_keyval = cleaned_string.split(",")
+
+dictionary = {}
+for keyval in arr_keyval:
+    temp_Arr = keyval.split(":")
+    dictionary[temp_Arr[0]] = float(temp_Arr[1]) if temp_Arr[1].isnumeric else temp_Arr[1]
+    
+print(dictionary[args[2]])
