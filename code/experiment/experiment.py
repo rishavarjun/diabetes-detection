82e98a7574dc4f9b7fbf6e539c8fb1e298659a10
namranpan <87070935+namranpan@users.noreply.github.com>
2021-09-30 15:24:02 +0530

    added experimentation workflow

---
+++ code/experiment/experiment.py
@@ -0,0 +1,60 @@
+import joblib
+import datetime
+from sklearn.ensemble import RandomForestClassifier
+
+from azureml.core import Workspace, Experiment, Dataset
+from azureml.core.model import Model
+from azureml.core.run import Run
+
+## --- Experiment Parameters ----##
+max_depth=2
+n_estimators = 20
+## ------------------------------##
+run = Run.get_context()
+ws = run.experiment.workspace
+
+if __name__ == "__main__":
+
+    # Loading registered model
+    production_model = ""
+
+    for model in Model.list(ws):
+        print(model.name, 'version:', model.version)
+        if "production" in model.tags.values():
+            production_model = model.name
+            break
+    
+    model_path = Model.get_model_path(production_model, _workspace=ws)
+    current_model = joblib.load(model_path)
+    experiment_model = RandomForestClassifier(n_estimators = n_estimators,max_depth = max_depth)
+
+    todays_date = datetime.datetime.today().strftime('%d/%b/%Y')
+    todays_date = todays_date.replace("/", "-")
+    fresh_ds = "diabetes_ds"
+
+    fresh_diabetes_ds = Dataset.get_by_name(ws, fresh_ds)
+    new_df = fresh_diabetes_ds.to_pandas_dataframe()
+    print(new_df.head())
+    array = new_df.values
+    x = array[:, 0:8]
+    # scaler = MinMaxScaler(feature_range =(0,1))
+    # rescaledx = scaler.fit_transform(x)
+
+    ground_truth = array[:, 8]
+    ground_truth = [int(i) for i in ground_truth]
+
+    predicted_label = current_model.predict(x)
+    predicted_label = [int(i) for i in predicted_label]
+        
+    correct = 0
+    for i, label in enumerate(ground_truth):
+        if ground_truth[i] == predicted_label[i]:
+            correct += 1
+
+    current_model_accuracy = correct/len(ground_truth)
+
+    experiment_model.fit(x,ground_truth)
+    experiment_accuracy = model.score(x,ground_truth)
+    print("current_model_accuracy", current_model_accuracy)
+    run.log("current_model_accuracy", current_model_accuracy)
+    run.log('New Experiment accuracy',experiment_accuracy)
