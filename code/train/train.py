8a0affeef1ff98ff9a407b5dab61b1bff643c443
namranpan <87070935+namranpan@users.noreply.github.com>
2022-08-05 06:26:03 -0500

    initializer edits

--- code/train/train.py
+++ code/train/train.py
@@ -1,32 +1,37 @@
-# from pandas import read_csv
 import numpy as np
-# from numpy import set_printoptions
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
 import joblib
 import datetime
-from sklearn.metrics import confusion_matrix
 import os
+
 from azureml.core import Workspace, Experiment, Dataset
 from azureml.core.run import Run
+
 run = Run.get_context()
 ws = run.experiment.workspace
 
+
+## --- Experiment Parameters ----##
+max_depth = 2
+n_estimators = 2
+## ------------------------------##
+
 if __name__ == "__main__":
 
-    diabetes_ds = Dataset.get_by_name(ws, 'diabetes-dataset')
-    df = diabetes_ds.to_pandas_dataframe()
+    fresh_ds = "diabetes-dataset"
 
-    # run = exp.start_logging()
-    # instead of printing as a standard out, divert them into azure logs
-    run.log("Experiment start time", str(datetime.datetime.now()))
+    fresh_diabetes_ds = Dataset.get_by_name(ws, fresh_ds)
+    df = fresh_diabetes_ds.to_pandas_dataframe()
 
-    # df = read_csv(file_path)
-    array = df.values
-
-    x = array[:, 0:8]
-    y = array[:, 8]
+    experiment_start_time = datetime.datetime.now()
+    
+    x = df.drop(columns='Outcome')
+    y = df['Outcome']
 
     scaler = MinMaxScaler(feature_range =(0,1))
     rescaledx = scaler.fit_transform(x)
@@ -36,19 +41,20 @@ if __name__ == "__main__":
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=7)
 
-    model = LogisticRegression(max_iter=100000)
-    model.fit(x_train, y_train)
-    result = model.score(x_test, y_test)
-    # run.log("Model result:", result)
-    
-    # outputs directory is automatically created so dump models here
+    experiment_model = RandomForestClassifier(n_estimators = n_estimators,max_depth = max_depth)
+    experiment_model.fit(x_train, y_train)
+    accuracy = experiment_model.score(x_test, y_test)
+   
     modelfile = 'outputs/model.pkl'
-    joblib.dump(model, modelfile)
+    joblib.dump(experiment_model, modelfile)
     
-    run.log('Intercept:', model.intercept_)
-    run.log('Slope:', model.coef_[0])
-    run.log("Experiment end time", str(datetime.datetime.now()))
-    run.log("Model result:", result)
+    experiment_end_time = datetime.datetime.now()
+    experiment_duration = (experiment_end_time - experiment_start_time).total_seconds()
+    
+    run.log('max_depth',max_depth )
+    run.log('n_estimators', n_estimators)
+    run.log("Experiment duration (s)", str(experiment_duration))
+    run.log('Experiment Model accuracy',accuracy)
     run.complete()
     
     print("Finished training!!")
